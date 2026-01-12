3c25639be6942a21e6301fc0a131b7788f5d451c
Mimouni Asmaa <asmaa@MacBook-Pro-de-Asmaa.local>
2026-01-12 17:58:21 +0100

    Deuxieme commit

--- srcCodeAPIs/mySecondAPI_with_MVC/app/views.py
+++ srcCodeAPIs/mySecondAPI_with_MVC/app/views.py
@@ -1,9 +1,18 @@
 from app import app
+
 from flask import render_template, request, jsonify
 
 ### EXO1 - simple API
 
+#@app.route('/')
+#def index():
+#    return "hello world"
+
 ### EXO2 - API with simple display
+
+@app.route('/')
+def index():
+    return  render_template('index.html')
 
 ### EXO3 - API with parameters display 
 
